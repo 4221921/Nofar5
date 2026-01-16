a14a8c8e9e9fb7a0a6f988b1468c6827e48565a1
4221921 <np2490@bs.amalnet.k12.il>
2026-01-16 19:32:54 +0200

    COMMIT1

--- app/src/main/java/com/example/nofar5/MainActivity.java
+++ app/src/main/java/com/example/nofar5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nofar5;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,4 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+   public void clickme(View view){
+        Button myButton = findViewById(R.id.btn);
+        myButton.setText("Oh, yea, i've been clicked");
+
+
+   }
+
+
+ }
+
+
+
+
+
